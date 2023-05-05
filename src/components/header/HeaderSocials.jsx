f23feb787720b64ee7cc27bd2d174be6ba0518de
Marcos A <aravenap@gmail.com>
2023-05-05 00:59:41 -0400

    added  rel=noreferrer to header links

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -6,9 +6,9 @@ import {BsFacebook} from 'react-icons/bs';
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
-        <a href="https://www.linkedin.com/in/marcos-aravena-9a257a15/" target='_blank'><BsLinkedin/></a>
-        <a href="https://github.com/msaravena" target='_blank'><FaGithub /></a>
-        <a href="https://www.facebook.com/marcos.s.aravena" target='_blank'><BsFacebook /></a>
+        <a href="https://www.linkedin.com/in/marcos-aravena-9a257a15/" target='_blank'  rel="noreferrer"><BsLinkedin/></a>
+        <a href="https://github.com/msaravena" target='_blank'  rel="noreferrer"><FaGithub /></a>
+        <a href="https://www.facebook.com/marcos.s.aravena" target='_blank'  rel="noreferrer"><BsFacebook /></a>
     </div>
   )
 }
